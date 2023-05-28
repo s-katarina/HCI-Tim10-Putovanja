c73fc64d8e028c632967d85a7eb22fc89f42cbff
SelenaMilutin <selena.milutin@gmail.com>
2023-05-29 00:07:11 +0200

    Fix bin

---
+++ Core/Database.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using HCI_Tim10_Putovanja.User;
+
+namespace HCI_Tim10_Putovanja.Core
+{
+	class Database
+	{
+		private List<AppUser> users = new List<AppUser>();
+
+		internal List<AppUser> Users { get => users; set => users = value; }
+
+		public Database()
+		{
+			users.Add(new AppUser("Pera", "peric", "pera@gmail.com", "e22212120", "22222", Role.AGENT));
+			users.Add(new AppUser("Nina", "peric", "nina@gmail.com", "e22212120", "22222", Role.PASSENGER));
+			users.Add(new AppUser("Mika", "peric", "mika@gmail.com", "e22212120", "22222", Role.PASSENGER));
+		}
+
+		public void AddUser(AppUser appUser) {
+			users.Add(appUser);
+		}
+	}
+}
