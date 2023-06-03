2ce84e2fd67242290ed0dcd9ac44b0246a909556
SelenaMilutin <selena.milutin@gmail.com>
2023-06-03 15:55:49 +0200

    Fix responsivness

---
+++ User/Converter/GreaterThanConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace HCI_Tim10_Putovanja.User.Converter
+{
+	public class GreaterThanConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return double.TryParse(value.ToString(), out double doubleValue) &&
+				double.TryParse(parameter.ToString(), out double doubleParametar) &&
+				doubleValue > doubleParametar;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return double.TryParse(value.ToString(), out double doubleValue) &&
+				double.TryParse(parameter.ToString(), out double doubleParametar) &&
+				doubleValue > doubleParametar;
+		}
+	}
+}
