9c485424d9b27b051111d9df584d12f605f9c1a8
miloss01 <mikicamiki.bat@gmail.com>
2023-06-11 16:34:25 +0200

    Add help for edit and create touristic stop pages

--- User/View/AllTouristicStops.xaml.cs
+++ User/View/AllTouristicStops.xaml.cs
@@ -51,5 +51,15 @@ namespace HCI_Tim10_Putovanja.User.View
             this.NavigationService.Navigate(page);
         }
 
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            IInputElement focusedControl = FocusManager.GetFocusedElement(Application.Current.Windows[0]);
+            if (focusedControl is DependencyObject)
+            {
+                string str = HelpProvider.GetHelpKey((DependencyObject)focusedControl);
+                HelpProvider.ShowHelp(str);
+            }
+        }
+
     }
 }
